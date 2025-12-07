8f00549fba7d559da971fafadcc39bd1c2fa8e83
lfr4nnl <franciscomaiajunior36@gmail.com>
2025-12-07 17:13:31 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('install', function(e){ self.skipWaiting(); });
-self.addEventListener('activate', function(e){ self.clients.claim(); });
-self.addEventListener('fetch', function(e){
-  // basic offline fallback - no caching strategy for simplicity
-});
