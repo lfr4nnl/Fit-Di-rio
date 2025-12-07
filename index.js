342bb8e9d8af435ed8c119e3876150ec11958e5c
lfr4nnl <franciscomaiajunior36@gmail.com>
2025-12-07 17:25:54 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import TreinoDiarioApp from './TreinoDiarioApp';
-
-import './index.css';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<TreinoDiarioApp />);
